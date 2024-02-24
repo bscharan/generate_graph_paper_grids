2bddecf6d2e7ede6bb2c95f6566b8e7e12159013
Sai Charan Bodda <bscharan1729@gmail.com>
2024-02-25 01:43:15 +0530

    used argparse and added help

--- generate_graph.py
+++ generate_graph.py
@@ -1,7 +1,48 @@
+import argparse, sys, os
+
 from PIL import Image, ImageDraw
 from math import floor
-import os
 
+width=0
+height=0
+sections=0
+sunsections=0
+padding=0
+tag=0
+# runDefaultFlag = False
+
+def parse_Arguments():
+    global width, height, sections, subsections, padding, tag
+
+    parser=argparse.ArgumentParser(description='Generate graph paper.')
+    # parser.add_argument('-d',"--default", action="store", help='run with default config, other parameters are ignored')
+    parser.add_argument("--width",action="store", default=1024, type=int, help="Width of the page in pixels, DefaultVaule: 1024")
+    parser.add_argument("--height",action="store", default=1024, type=int, help="height of the page in pixels, DefaultVaule: 1024")
+    parser.add_argument("--sections",action="store", default=10, type=int, help="number of section, the width should be divided, DefaultVaule: 10")
+    parser.add_argument("--subsections",action="store", default=10, type=int, help="number of subsections each section should be divided, DefaultVaule: 10")
+    parser.add_argument("--padding",action="store", default=12, type=int, help="outer margins width in pixels for the graph, , DefaultVaule: 12")
+    parser.add_argument("--tag",action="store", default="default", help="suffix for output file name, DefaultVaule: default")
+    args=parser.parse_args()
+
+    
+    if args.width:
+        width = args.width
+    
+    if args.height:
+        height = args.height
+    
+    if args.sections:
+        sections = args.sections
+    
+    if args.subsections:
+        subsections = args.subsections
+
+    if args.padding:
+        padding = args.padding
+    
+    if args.tag:
+        tag = args.tag
+    
 
 def create_graph_paper(width, height, sections, subsections, padding):
       
@@ -53,9 +94,6 @@ def create_graph_paper(width, height, sections, subsections, padding):
     return image
 
 
-directory = "./output"
-if not os.path.exists(directory):
-    os.makedirs(directory)
 
 
 # TODO Add def dotted_line(): to create a dotted line.
@@ -65,9 +103,33 @@ def generate_graph(width, height, padding, sections, subsections, tag):
     graph_paper = create_graph_paper(width, height, sections, subsections, padding)
     graph_paper.save(f"./output/graph_paper_{tag}.png")
 
-generate_graph(640, 1000, 10, 10, 10, "sm")
-generate_graph(768, 1000, 4, 10, 10, "md")
-generate_graph(1024, 1280, 12, 10, 10, "lg")
-generate_graph(1280, 1280, 10, 10, 10, "xl")
-generate_graph(1536, 1280, 8, 10, 10, "2xl")
+def runDefault():
+    
+    generate_graph(640, 1000, 10, 10, 10, "sm")
+    generate_graph(768, 1000, 4, 10, 10, "md")
+    generate_graph(1024, 1280, 12, 10, 10, "lg")
+    generate_graph(1280, 1280, 10, 10, 10, "xl")
+    generate_graph(1536, 1280, 8, 10, 10, "2xl")
+
+def main():
+    directory = "./output"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    if not len(sys.argv) > 1:
+        print("""
+        running with default config,
+        generating graphs for sm,md,lg,xl,2xl sizes
+        run following command to view all options
+            >> python generate_graph.py -h  """)
+        runDefault()
+    else: 
+        parse_Arguments()
+        print(f"""
+        creating graph with Width  = {width}, height = {height}, padding = {padding},
+        sections = {sections}, subsections = {subsections}, output_file = ./output/graph_paper_{tag}.png""")
+        generate_graph(width, height, padding, sections, subsections, tag)
+
+
+main()
 

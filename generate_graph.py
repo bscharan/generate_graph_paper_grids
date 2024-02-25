94a314c9d4b41f9eb9e2b26dde0dd0d32514ae31
Sai Charan Bodda <bscharan1729@gmail.com>
2024-02-25 18:30:52 +0530

    fix scripts

--- generate_graph.py
+++ generate_graph.py
@@ -56,12 +56,13 @@ def create_graph_paper(width, height, sections, subsections, padding):
     minor_gap = major_gap/subsections
 
     # Draw horizontal lines
+    top_padding = 10
     pos = 0
     x = 0
     while pos < height:
         for y in range (0, subsections):
             line_width = 1
-            pos = floor(padding + x*major_gap + y*minor_gap)
+            pos = floor(top_padding + x*major_gap + y*minor_gap)
             if y%5 == 0 and y!=0:
                 pos = pos - 1
                 line_width = 2
@@ -87,7 +88,7 @@ def create_graph_paper(width, height, sections, subsections, padding):
                 if x%5 == 0:
                     line_width = 4
             # print("Pos: ", pos, " LineWidth: ", line_width)
-            draw.line([(pos, padding), (pos, height + padding)], fill="#36454f", width=line_width)
+            draw.line([(pos, top_padding), (pos, height + top_padding)], fill="#36454f", width=line_width)
             if x == sections:
                 break
 
@@ -106,7 +107,7 @@ def generate_graph(width, height, padding, sections, subsections, tag):
 def runDefault():
     
     generate_graph(640, 1000, 10, 10, 10, "sm")
-    generate_graph(768, 1000, 4, 10, 10, "md")
+    generate_graph(768, 1000, 9, 10, 10, "md")
     generate_graph(1024, 1280, 12, 10, 10, "lg")
     generate_graph(1280, 1280, 10, 10, 10, "xl")
     generate_graph(1536, 1280, 8, 10, 10, "2xl")
